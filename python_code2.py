64e7cf6e1cae67c0ebfafbc96ccf939f465d8524
Vanessa Vallejo Khoury <vanessavallejo@MacBook-Pro.lan>
2019-08-01 17:35:21 +0200

    adds a python script that imports pandas, numpy and greets the Coursera Capstone

---
+++ python_code2.py
@@ -0,0 +1,6 @@
+#This notebook will be used for the Coursera Capstone Project, as the final assignment for the Data Science Course.
+
+import pandas as pd
+import numpy as np
+print ('Hello Capstone Project Course!')
+
